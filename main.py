1cf45e168fb14e74ff6f7ac48251db996f48cf61
Alexandre Ramirez <ps78ppi@eduvaud.ch>
2025-11-14 10:51:10 +0100

    change to rectangular cases

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import pygame
 pygame.init()
 
 #setup pygame
-screen = pygame.display.set_mode((1400, 800),pygame.RESIZABLE) #,pygame.display.set_caption('Ludo Game')
+screen = pygame.display.set_mode((1400, 800),pygame.RESIZABLE)
 screen_width, screen_height = screen.get_size()
 
-#setup window's title
 pygame.display.set_caption("Ludo Game")
 
 # define game variable
 ROWS, COLS = 15, 15
-CELL = 40
+CELL_WIDTH = 80
+CELL_HEIGHT = 40
 
 #colors
 RED = (255, 0, 0)
@@ -22,74 +22,73 @@ YELLOW = (255, 255, 0)
 BLACK = (0, 0, 0)
 
 class Cell:
-    def __init__(self, row, col, size, id):
+    def __init__(self, row, col, width, height, id):
         self.row = row
         self.col = col
-        self.size = size
+        self.width = width
+        self.height = height
         self.id = id
         self.color = WHITE
 
     def draw(self, surface):
-        x = self.col * self.size
-        y = self.row * self.size
-        pygame.draw.rect(surface, self.color, (x, y, self.size, self.size))
-        pygame.draw.rect(surface, BLACK, (x , y, self.size, self.size), 1)
+        x = self.col * self.width
+        y = self.row * self.height
+        pygame.draw.rect(surface, self.color, (x, y, self.width, self.height))
+        pygame.draw.rect(surface, BLACK, (x, y, self.width, self.height), 1)
 
-#create the grill + id
+# create the grid + id
 grid = []
 id_counter = 0
 
 for row in range(ROWS):
     grid.append([])
     for col in range(COLS):
-        grid[row].append(Cell(row, col, CELL, id_counter))
+        grid[row].append(Cell(row, col, CELL_WIDTH, CELL_HEIGHT, id_counter))
         id_counter += 1
 
-
 def color_ludo():
-    #zone rouge
-    for r in range(0,6):
-        for c in range(0,6):
+    # zone rouge
+    for r in range(0, 6):
+        for c in range(0, 6):
             grid[r][c].color = RED
 
-    for r in range(0,6):
-        for c in range(9,15):
+    # zone bleue
+    for r in range(0, 6):
+        for c in range(9, 15):
             grid[r][c].color = BLUE
 
-    for r in range(9,15):
-        for c in range(9,15):
+    # zone jaune
+    for r in range(9, 15):
+        for c in range(9, 15):
             grid[r][c].color = YELLOW
 
-    for r in range(9,15):
-        for c in range(0,6):
+    # zone verte
+    for r in range(9, 15):
+        for c in range(0, 6):
             grid[r][c].color = GREEN
 
 color_ludo()
 
-#laod image
-
-
 run = True
 
-#main loop
 while run:
 
     events = pygame.event.get()
     for event in events:
-        #quit pygame
+
         if event.type == pygame.QUIT:
             run = False
 
-        #Cliquer : Montre l'id de la case
+        # click detection
         if event.type == pygame.MOUSEBUTTONDOWN:
             mx, my = pygame.mouse.get_pos()
-            col = mx // CELL
-            row = mx // ROWS
-            print("Case cliqué -> id =", grid[row][col].id)
+            col = mx // CELL_WIDTH
+            row = my // CELL_HEIGHT
+            print("Case cliquée -> id =", grid[row][col].id)
 
-    screen.fill((0,0,0))
+    screen.fill((0, 0, 0))
 
-    #dessiner toutes les cases
+    # draw all cells
     for row in grid:
         for cell in row:
             cell.draw(screen)
