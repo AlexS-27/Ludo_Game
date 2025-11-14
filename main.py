101bc6269976d1b6f31fb4e810688ca49d7298ae
Alex Kamano <pk86onf@eduvaud.ch>
2025-11-14 10:24:35 +0100

    commit before create a new feature

--- main.py
+++ main.py
@@ -10,7 +10,64 @@ screen_width, screen_height = screen.get_size()
 pygame.display.set_caption("Ludo Game")
 
 # define game variable
+ROWS, COLS = 15, 15
+CELL = 40
+
+#colors
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+BLACK = (0, 0, 0)
+
+class Cell:
+    def __init__(self, row, col, size, id):
+        self.row = row
+        self.col = col
+        self.size = size
+        self.id = id
+        self.color = WHITE
+
+    def draw(self, surface):
+        x = self.col * self.size
+        y = self.row * self.size
+        pygame.draw.rect(surface, self.color, (x, y, self.size, self.size))
+        pygame.draw.rect(surface, BLACK, (x , y, self.size, self.size), 1)
+
+#create the grill + id
+grid = []
+id_counter = 0
+
+for row in range(ROWS):
+    grid.append([])
+    for col in range(COLS):
+        grid[row].append(Cell(row, col, CELL, id_counter))
+        id_counter += 1
+
+
+def color_ludo():
+    #zone rouge
+    for r in range(0,6):
+        for c in range(0,6):
+            grid[r][c].color = RED
+
+    for r in range(0,6):
+        for c in range(9,15):
+            grid[r][c].color = BLUE
+
+    for r in range(9,15):
+        for c in range(9,15):
+            grid[r][c].color = YELLOW
+
+    for r in range(9,15):
+        for c in range(0,6):
+            grid[r][c].color = GREEN
+
+color_ludo()
+
 #laod image
+
 
 run = True
 
@@ -23,4 +80,20 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    pygame.display.update()
+        #Cliquer : Montre l'id de la case
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mx, my = pygame.mouse.get_pos()
+            col = mx // CELL
+            row = mx // ROWS
+            print("Case cliqué -> id =", grid[row][col].id)
+
+    screen.fill((0,0,0))
+
+    #dessiner toutes les cases
+    for row in grid:
+        for cell in row:
+            cell.draw(screen)
+
+    pygame.display.flip()
+
+pygame.quit()
